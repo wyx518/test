42b7a66e5e30af7cf5bdef3ab8ae99232a6ef14e
wyx518 <15808006814@163.com>
2018-09-27 20:25:01 +0800

    42

---
+++ 42-sa.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] heights) {
+        int left = 0, right = heights.length - 1; 
+        int res = 0;
+        if(left >= right)
+            return res;
+        int leftheight = heights[left];
+        int rightheight = heights[right];
+        while(left < right) {
+            if(leftheight < rightheight) {
+                left ++;
+                if(leftheight > heights[left]) {
+                    res += (leftheight - heights[left]);
+                } else {
+                    leftheight = heights[left];
+                }
+            } else {
+                right --;
+                if(rightheight > heights[right]) {
+                    res += (rightheight - heights[right]);
+                } else {
+                    rightheight = heights[right];
+                }
+            }
+        }
+        return res;
+    }
+}
